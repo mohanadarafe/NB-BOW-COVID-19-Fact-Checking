0547621b8bd2328ced06c24658fa28c8bc89b070
mohanadarafe <marafe@live.com>
2020-12-11 00:00:23 -0500

    Small refactor of predict method for classifier
    Co-authored-by: ribal-aladeeb <aladeeb.r@hotmail.com>

--- naive_bayes.py
+++ naive_bayes.py
@@ -7,8 +7,8 @@ class NaiveBayes:
         self.fileName = fileName
         self.vocabulary = vocabulary.originalVocabulary(fileName) if isOriginal else vocabulary.filteredVocabulary(fileName)
         self.vocabularySize = len(self.vocabulary)
-        self.numberOfFactual = utils.total_yes_no(fileName)
-        self.numberOfNonFactual = utils.total_yes_no(fileName, False)
+        self.yesPrior = utils.total_yes_no(fileName) / (utils.total_yes_no(fileName) + utils.total_yes_no(fileName, False))
+        self.noPrior = utils.total_yes_no(fileName, False) / (utils.total_yes_no(fileName) + utils.total_yes_no(fileName, False))
         self.numberOfYes = utils.total_word_in_class(self.vocabulary)
         self.numberOfNo = utils.total_word_in_class(self.vocabulary, False)
 
@@ -21,17 +21,15 @@ class NaiveBayes:
         trace_file_results = []
 
         for tweet in data:
-            yes_score = 0
-            no_score = 0
-            for tokens in tweet[1].split(" "):
-                if tokens in self.vocabulary:
-                    yes_score += self.score(tokens, 'yes')
-                    no_score += self.score(tokens, 'no')
-
+            tokensList = tweet[1].split(" ")
+            yes_score = math.log10(self.yesPrior) + sum([math.log10(self.conditional(token, 'yes')) for token in tokensList])
+            no_score = math.log10(self.noPrior) + sum([math.log10(self.conditional(token, 'no')) for token in tokensList])
+            
             best_score = yes_score if yes_score > no_score else no_score
             prediction = 'yes' if yes_score > no_score else 'no'
             true_value = tweet[2]
             label = 'correct' if prediction == true_value else 'wrong'
+
             if prediction == true_value:
                 correct_prediction+=1
 
@@ -39,27 +37,13 @@ class NaiveBayes:
             
         print(correct_prediction/len(data))
 
-    def score(self, word: str, flag: str):
-        '''
-        Computes the score of a word
-        Score(flag) = log(P(flag)) + log(P(word|flag))
-        '''
-        P = self.P(self.numberOfFactual, self.numberOfNonFactual) if flag == 'yes' else self.P(self.numberOfNonFactual, self.numberOfFactual)
-        return math.log(P) + math.log10(self.conditional(word, flag))
-
-    def P(self, num1: int, num2: int) -> float:
-        '''
-        Computes the probability of the class.
-        '''
-        return num1 / (num1 + num2)
-
     def conditional(self, word: str, flag: str):
         '''
         Computes the conditional probability of a word given its class.
         '''
         wordOccurencesInClass = self.vocabulary[word][0] if flag == 'yes' else self.vocabulary[word][1]
         totalOccurencesInClass = self.numberOfYes if flag == 'yes' else self.numberOfNo
-        return ((wordOccurencesInClass + self.smoothing) / (totalOccurencesInClass + (self.vocabularySize * self.smoothing)))
+        return (wordOccurencesInClass + self.smoothing) / (totalOccurencesInClass + (self.vocabularySize * self.smoothing))
 
 
 TRAINING_FILE = "data/covid_training.tsv"
